355920b3ad707781648f866509d3e3ba25b63b27
João Bispo <joaobispo2077@gmail.com>
2021-08-26 18:28:30 -0300

    feat: add Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const Home = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Testing</Text>
+      <Text>Native</Text>
+    </View>
+  );
+};

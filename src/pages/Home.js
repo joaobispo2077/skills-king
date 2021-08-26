199b43120dbb24f634580bb8cdfb917d50ad28ba
João Bispo <joaobispo2077@gmail.com>
2021-08-26 18:44:45 -0300

    feat: add base styles into app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,60 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 
 export const Home = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Testing</Text>
-      <Text>Native</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Welcome, João</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
+      />
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+      <Text style={[styles.title, { marginTop: 50 }]}>My skills</Text>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 30,
+    paddingVertical: 70,
+  },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  input: {
+    color: '#fff',
+    fontSize: 18,
+    backgroundColor: '#1F1E25',
+    padding: Platform.OS === 'ios' ? 15 : 12,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+});
